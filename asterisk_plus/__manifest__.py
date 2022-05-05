741d0a68c28c54e2a07164d54a90394084e4b761
Andro Chvertkini <chvertkini.andro@gmail.com>
2022-05-05 14:25:02 +0400

    add_note -> set_notes wizard renamed

--- asterisk_plus/__manifest__.py
+++ asterisk_plus/__manifest__.py
@@ -50,7 +50,7 @@
         # Cron
         'views/ir_cron.xml',
         # Wizards
-        'wizard/add_note.xml',
+        'wizard/set_notes.xml',
         'wizard/call.xml',
         # Reports
         'reports/reports.xml',
--- asterisk_plus/wizard/set_notes.py
+++ asterisk_plus/wizard/set_notes.py
@@ -2,12 +2,12 @@ from odoo import models, fields
 
 
 class AddNoteWizard(models.TransientModel):
-    _name = 'asterisk_plus.add_note_wizard'
+    _name = 'asterisk_plus.set_notes_wizard'
     _description = 'Add notes to call'
 
     notes = fields.Html()
 
-    def add_note(self):
+    def set_notes(self):
         call = self.env['asterisk_plus.call'].browse(
             self.env.context['active_ids'])
         call.notes = self.notes
