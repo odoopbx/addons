666a7e99d97f9fd78042439a22a4ef28ee91b21f
Andro Chvertkini <chvertkini.andro@gmail.com>
2022-05-07 15:19:49 +0400

    create portal user for a customer

--- asterisk_plus/controllers/paddle.py
+++ asterisk_plus/controllers/paddle.py
@@ -1,6 +1,6 @@
 import logging
 from odoo import http
-from odoo.http import Response
+from odoo.http import request, Response
 
 
 logger = logging.getLogger(__name__)
@@ -9,6 +9,25 @@ logger = logging.getLogger(__name__)
 class PaddleController(http.Controller):
 
     @http.route('/asterisk_plus_support/paddle', type='http', auth='none', csrf=False)
-    def product_purchase(self, **kwargs):
-        logger.info(kwargs)
+    def paddle_alert(self, **kwargs):
+        """Alerts from paddle."""
+        if kwargs.get('alert_name') == 'payment_succeeded':
+            logger.info('Paddle payment succeeded')
+            self.create_user(kwargs)
         return Response('success', status=200)
+
+    def create_user(self, kwargs):
+        """Create portal user for a customer."""
+
+        request.env['res.users'].with_context({'mail_create_nolog': True}).sudo().create({
+                'name': kwargs.get('customer_name'),
+                'login': kwargs.get('email'),
+                'email': kwargs.get('email'),
+                'password': 'portal',
+                'company_ids': [request.env.ref('base.main_company').id],
+                'company_id': request.env.ref('base.main_company').id,
+                'groups_id': [(6, 0, [request.env.ref('base.group_portal').id])],
+            })
+
+        logger.info('Portal user created. Name: {}, Email: {}'.format(
+            kwargs.get('customer_name'), kwargs.get('email')))
