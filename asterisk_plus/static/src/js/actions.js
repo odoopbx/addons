a6245b724f0916dd4dea6adc5c824056e0e8a1a4
Nicolae Postica <nicolae.postica2@gmail.com>
2022-05-12 18:55:46 +0300

    [asterisk_plus] fix actions.js

--- asterisk_plus/static/src/js/actions.js
+++ asterisk_plus/static/src/js/actions.js
@@ -1,119 +1,96 @@
-odoo.define("asterisk_plus.actions", function (require) {
-    "use strict";
+/** @odoo-module **/
 
-    var AbstractService = require('web.AbstractService');
-    var ajax = require('web.ajax');
-    var session = require('web.session');
-    var personal_channel = 'asterisk_plus_actions_' + session.uid;
-    var common_channel = 'asterisk_plus_actions';
+import {registry} from "@web/core/registry";
+import {uid} from "web.session";
+import {useService} from "@web/core/utils/hooks";
 
-    var AjaxService = AbstractService.extend({
-        name: 'ajax',
-        start: function () {
-            this._super()
-            let self = this
-            ajax.rpc('/web/dataset/call_kw/asterisk_plus', {
-                "model": "asterisk_plus.user",
-                "method": "has_asterisk_plus_group",
-                "args": [],
-                "kwargs": {},
-            }).then(function (res) {
-                if (res == true) {
-                    ajax.rpc('/web/dataset/call_kw/res.users', {
-                        "model": "res.users",
-                        "method": "get_pbx_user_settings",
-                        "args": [session.uid],
-                        "kwargs": {},
-                    }).then(function (settings) {
-                        console.log(settings)
-                    })
-                    // Start polling
-                    self.call('bus_service', 'addChannel', personal_channel);
-                    self.call('bus_service', 'addChannel', common_channel);
-                    self.call('bus_service', 'onNotification', self, self.on_asterisk_plus_action)
-                    // console.log('Listening on Asterisk Plus actions')
-                }
-            })
-        },
+var personal_channel = 'asterisk_plus_actions_' + uid;
+var common_channel = 'asterisk_plus_actions';
 
-        _trigger_up: function (ev) {},
+export const actionService = {
+    dependencies: ["action", "notification", "rpc"],
 
-        on_asterisk_plus_action: function (action) {
-            for (var i = 0; i < action.length; i++) {
-                var ch = action[i][0]
-                var msg = action[i][1]
-                if (ch == personal_channel || ch == common_channel) {
-                    try {
-                        this.asterisk_plus_handle_action(msg)
-                    } catch (err) {
-                        console.log(err)
-                    }
+    start(env, {action, notification, rpc}) {
+        const legacyEnv = owl.Component.env;
+        legacyEnv.services.bus_service.addChannel(personal_channel);
+        legacyEnv.services.bus_service.addChannel(common_channel);
+        legacyEnv.services.bus_service.onNotification(this, this.on_asterisk_plus_action);
+        legacyEnv.services.bus_service.startPolling();
+
+        this.notification = useService("notification");
+    },
+
+    on_asterisk_plus_action: function (action) {
+        for (var i = 0; i < action.length; i++) {
+            var {type, payload} = action[i];
+            if (type == 'asterisk_plus_notify') {
+                try {
+                    this.asterisk_plus_handle_action(payload);
+                } catch (err) {
+                    console.log(err);
                 }
             }
-        },
+        }
+    },
 
-        asterisk_plus_handle_action: function (msg) {
-            console.log(msg)
-            if (typeof msg == 'string')
-                var message = JSON.parse(msg)
-            else
-                var message = msg
-            // Check if this is a reload action.
-            if (message.action == 'reload_view') {
-                return this.asterisk_plus_handle_reload_view(message)
-            }
-            // Check if this is a notification action
-            else if (message.action == 'notify') {
-                return this.asterisk_plus_handle_notify(message)
-            }
-            // Check if it a open record action
-            else if (message.action == 'open_record') {
-                return this.asterisk_plus_handle_open_record(message)
-            }
-        },
+    asterisk_plus_handle_action: function (msg) {
+        // console.log(msg)
+        if (typeof msg == 'string')
+            var message = JSON.parse(msg)
+        else
+            var message = msg
+        // Check if this is a reload action.
+        if (message.action == 'reload_view') {
+            return this.asterisk_plus_handle_reload_view(message)
+        }
+        // Check if this is a notification action
+        else if (message.action == 'notify') {
+            return this.asterisk_plus_handle_notify(message)
+        }
+        // Check if it a open record action
+        else if (message.action == 'open_record') {
+            return this.asterisk_plus_handle_open_record(message)
+        }
+    },
 
-        asterisk_plus_handle_open_record: function (message) {
-            // console.log('Opening record form')
-            this.do_action({
-                'type': 'ir.actions.act_window',
-                'res_model': message.model,
-                'target': 'current',
-                'res_id': message.res_id,
-                'views': [[message.view_id, 'form']],
-                'view_mode': 'tree,form',
-            })
-        },
+    asterisk_plus_handle_open_record: function (message) {
+        // console.log('Opening record form')
+        this.do_action({
+            'type': 'ir.actions.act_window',
+            'res_model': message.model,
+            'target': 'current',
+            'res_id': message.res_id,
+            'views': [[message.view_id, 'form']],
+            'view_mode': 'tree,form',
+        })
+    },
 
-        asterisk_plus_handle_reload_view: function (message) {
-            var action = this.action_manager && this.action_manager.getCurrentAction()
-            if (!action) {
-                // console.log('Action not loaded')
-                return
-            }
-            var controller = this.action_manager.getCurrentController()
-            if (!controller) {
-                // console.log('Controller not loaded')
-                return
-            }
-            if (controller.widget.modelName != message.model) {
-                // console.log('Not message model view')
-                return
-            }
-            // console.log('Reload')
-            controller.widget.reload()
-        },
+    asterisk_plus_handle_reload_view: function (message) {
+        var action = this.action_manager && this.action_manager.getCurrentAction()
+        if (!action) {
+            // console.log('Action not loaded')
+            return
+        }
+        var controller = this.action_manager.getCurrentController()
+        if (!controller) {
+            // console.log('Controller not loaded')
+            return
+        }
+        if (controller.widget.modelName != message.model) {
+            // console.log('Not message model view')
+            return
+        }
+        // console.log('Reload')
+        controller.widget.reload()
+    },
 
-        asterisk_plus_handle_notify: function ({title, message, sticky, warning}) {
-            console.log({title, message, sticky, warning})
-            if (warning == true)
-                this.displayNotification({title, message, sticky, type: 'danger'})
-            else
-                this.displayNotification({title, message, sticky, type: 'warning'})
-        },
-    })
+    asterisk_plus_handle_notify: function ({title, message, sticky, warning}) {
+        // console.log({title, message, sticky, warning})
+        if (warning == true)
+            this.notification.notify({title, message, sticky, type: 'danger'})
+        else
+            this.notification.notify({title, message, sticky, type: 'warning'})
+    },
+}
 
-    owl.utils.whenReady().then(() => {
-        const service = new AjaxService();
-        service.start();
-    });
-})
+registry.category("services").add("actionService", actionService);
