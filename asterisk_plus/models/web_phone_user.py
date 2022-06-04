000e4de68545b5e3a63a42f55984eb89860a35fd
Alex Litni <litnialex@gmail.com>
2022-06-04 20:20:04 +0300

    clean from sip autocreate

--- asterisk_plus/models/web_phone_user.py
+++ asterisk_plus/models/web_phone_user.py
@@ -12,35 +12,3 @@ class WebPhoneUser(models.Model):
 
     web_phone_sip_user = fields.Char(string="SIP User")
     web_phone_sip_secret = fields.Char(string="SIP Secret")
-    is_sip_autocreate_enabled = fields.Char(compute='_compute_sip_autocreate_enabled')
-
-    @api.model
-    def create(self, values):
-        user = super(WebPhoneUser, self).create(values)
-        debug(self, f"Created user {user.login}")
-        # create SIP account if enabled
-        if self.env['asterisk_plus.settings'].sudo().get_param('auto_create_sip_peers'):
-            if hasattr(self.env['asterisk_plus.user'], 'auto_create'):
-                self.env['asterisk_plus.user'].auto_create(user)
-        return user
-
-    def write(self, vals):
-        debug(self, f"Updating user {self.login} web_phone settings")
-        res = super(WebPhoneUser, self).write(vals)
-        if 'web_phone_sip_user' in vals or 'web_phone_sip_secret' in vals:
-            if not self.env.context.get('skip_update_config'):
-                if hasattr(self.env['asterisk_plus.user'], 'generate_configs'):
-                    self.env['asterisk_plus.user'].generate_configs()
-        return res
-
-    def unlink(self):
-        res = super(WebPhoneUser, self).unlink()
-        if hasattr(self.env['asterisk_plus.user'], 'generate_configs'):
-            self.env['asterisk_plus.user'].generate_configs()
-        return res
-
-    def _compute_sip_autocreate_enabled(self):
-        for rec in self:
-            rec.is_sip_autocreate_enabled = self.env[
-                'asterisk_plus.settings'].sudo().get_param('auto_create_sip_peers')
-
