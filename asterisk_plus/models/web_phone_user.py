52155c7ac74b5314893a50e09f05b7c00374b588
litnimax <litnimaxster@gmail.com>
2022-07-04 21:30:00 +0300

    Webphone user fix

--- asterisk_plus/models/web_phone_user.py
+++ asterisk_plus/models/web_phone_user.py
@@ -3,15 +3,21 @@ import logging
 from odoo import models, fields, api
 from .settings import debug
 from odoo.addons import base
-base.models.res_users.USER_PRIVATE_FIELDS.append('web_phone_sip_user')
-base.models.res_users.USER_PRIVATE_FIELDS.append('web_phone_sip_secret')
 
 logger = logging.getLogger(__name__)
 
+WEB_PHONE_USER_FIELDS = ['web_phone_sip_user', 'web_phone_sip_secret']
 
 class WebPhoneUser(models.Model):
     _inherit = 'res.users'
-    _description = "Web Phone"
 
     web_phone_sip_user = fields.Char(string="SIP User")
     web_phone_sip_secret = fields.Char(string="SIP Secret")
+
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + WEB_PHONE_USER_FIELDS
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + WEB_PHONE_USER_FIELDS
